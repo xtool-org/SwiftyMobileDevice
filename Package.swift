35bfb6b02513561b332e198df6cfb359eb75d922
Kabir Oberai <oberai.kabir@gmail.com>
2024-11-17 18:02:57 -0500

    Update SuperchargeCore remote

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/kabiroberai/SuperchargeCore", .upToNextMinor(from: "1.1.0")),
+        .package(url: "https://github.com/SuperchargeApp/SuperchargeCore", .upToNextMinor(from: "1.1.0")),
     ],
     targets: [
         .target(
