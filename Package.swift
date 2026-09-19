1f6e4cc614a31953bda5d1092a747cfdea90916f
Kabir Oberai <oberai.kabir@gmail.com>
2026-09-19 17:11:34 -0400

    Use automatic library type

--- Package.swift
+++ Package.swift
@@ -2,16 +2,6 @@
 
 import PackageDescription
 
-extension Product.Library.LibraryType {
-    static var smart: Self {
-        #if os(Linux)
-        return .static
-        #else
-        return .dynamic
-        #endif
-    }
-}
-
 let package = Package(
     name: "SwiftyMobileDevice",
     platforms: [
@@ -21,12 +11,11 @@ let package = Package(
     products: [
         .library(
             name: "SwiftyMobileDevice",
-            type: .smart,
             targets: ["SwiftyMobileDevice"]
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/xtool-org/xtool-core", .upToNextMinor(from: "1.4.0")),
+        .package(url: "https://github.com/xtool-org/xtool-core", .upToNextMinor(from: "1.5.0")),
     ],
     targets: [
         .target(
